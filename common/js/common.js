87274ad5c4f872d58bcd629bed74d7ea864a516b
livelybone <2631541504@qq.com>
2018-12-19 10:16:19 +0800

    update

--- common/js/common.js
+++ common/js/common.js
@@ -1,25 +1,27 @@
-function isMobile() {
+function isMobileFn() {
   return /Android|webOS|iPhone|iPod|iPad|BlackBerry|Windows Phone/i.test(navigator.userAgent)
 }
 
-function scale(noScale) {
+function setViewport(noScale) {
+  window.isMobile = isMobileFn()
   // canScale用来应付特殊情况
-  if (isMobile()) { // 判断访问环境是 Android|webOS|iPhone|iPod|BlackBerry(移动端)
-    var scale = window.devicePixelRatio || 1;
-    var content = 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no';
-    if (!noScale && scale !== 1) {
-      document.documentElement.style.fontSize = 625 * scale + '%';
-      content = 'width=device-width, initial-scale=' + 1 / scale + ', minimum-scale=' + 1 / scale + ', maximum-scale=' + 1 / scale + ', user-scalable=no';
-    }
-    var meta = document.createElement('meta');
-    meta.setAttribute('name', 'viewport');
-    meta.setAttribute('content', content);
-    document.head.appendChild(meta);
+  var scale = window.isMobile && !noScale ? window.devicePixelRatio || 1 : 1
+  var content = 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no'
+
+  document.documentElement.style.fontSize = 625 * scale + '%'
+  window.rootFootSize = { value: 100 * scale, unit: 'px/rem' }
+  if (scale !== 1) {
+    var s = 1 / scale
+    content = 'width=device-width, initial-scale=' + s + ', minimum-scale=' + s + ', maximum-scale=' + s + ', user-scalable=no'
   }
+  var meta = document.createElement('meta')
+  meta.setAttribute('name', 'viewport')
+  meta.setAttribute('content', content)
+  document.head.appendChild(meta)
 }
 
 function getEl(selector) {
   return document.querySelectorAll(selector)
 }
 
-scale(/noScale/i.test(location.href));
+setViewport(/noScale/i.test(location.href))
